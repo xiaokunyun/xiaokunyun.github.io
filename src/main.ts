e06b816cc81957c8a6b38626524ef459751a5975
xuxiaokun <xuxiaokun@cnrmall.com>
2022-07-22 17:26:13 +0800

    1

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,9 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import 'virtual:windi-devtools'
+const app=createApp(App)
+import router from "./router";
+app.use(router)
 
-createApp(App).mount('#app')
+app.mount('#app')
