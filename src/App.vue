a0ab998224879e801685d00d30fe78f4382f4259
xuxiaokun <xuxiaokun@cnrmall.com>
2022-07-22 15:08:13 +0800

    1

--- src/App.vue
+++ src/App.vue
@@ -12,9 +12,6 @@ import HelloWorld from './components/HelloWorld.vue'
     <a href="https://vuejs.org/" target="_blank">
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
   </div>
   <HelloWorld msg="Vite + Vue" />
 </template>
