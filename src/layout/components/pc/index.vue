eef97c534804ccfc8de6b3c93d4735dcb8e336d1
xuxiaokun <xuxiaokun@cnrmall.com>
2022-08-23 18:00:58 +0800

    8

--- src/layout/components/pc/index.vue
+++ src/layout/components/pc/index.vue
@@ -23,7 +23,7 @@
       <p>{{ $t('message.hello', ['hello']) }}</p>
       <video width="320" height="240" controls>
         <source src="@/assets/video/github.mp4" type="video/mp4" />
-        <source src="movie.ogg" type="video/ogg" />
+        <!-- <source src="movie.ogg" type="video/ogg" /> -->
         Your browser does not support the video tag.
       </video>
       <router-view></router-view>
