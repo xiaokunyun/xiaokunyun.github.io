9e5b3092dd7b6ec8040cedeade17a2f3a0e41393
xuxiaokun <xuxiaokun@cnrmall.com>
2022-09-21 11:41:46 +0800

    9

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,11 @@
 <script setup lang="ts">
 import en from '@arco-design/web-vue/es/locale/lang/en-us';
-import zh from '@arco-design/web-vue/es/locale/lang/zh-CN';
+import zhCN from '@arco-design/web-vue/es/locale/lang/zh-CN';
 import { useStore } from '@/store/user'
 console.log("useStore().locales",useStore().locales);
-
 </script>
 <template>
-  <a-config-provider :locale="zh">
+  <a-config-provider :locale="zhCN">
     <RouterView />
   </a-config-provider>
 </template>
