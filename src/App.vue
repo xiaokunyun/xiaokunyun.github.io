02d91c75d4efd5362ed7ec849301a165611591d9
xuxiaokun <xuxiaokun@cnrmall.com>
2022-09-21 11:47:04 +0800

    10

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,18 @@
 <script setup lang="ts">
-import en from '@arco-design/web-vue/es/locale/lang/en-us';
-import zhCN from '@arco-design/web-vue/es/locale/lang/zh-CN';
+import enUS from '@arco-design/web-vue/es/locale/lang/en-us';
+import zhCN from '@arco-design/web-vue/es/locale/lang/zh-cn';
 import { useStore } from '@/store/user'
-console.log("useStore().locales",useStore().locales);
+console.log("useStore().locales", useStore().locales);
+const locale = computed(() => {
+  switch (useStore().locales) {
+    case 'zh':
+      return zhCN;
+    case 'en':
+      return enUS;
+    default:
+      return zhCN;
+  }
+});
 </script>
 <template>
   <a-config-provider :locale="zhCN">
--- src/store/user.ts
+++ src/store/user.ts
@@ -1,12 +1,10 @@
 import { defineStore } from 'pinia'
-import en from '@arco-design/web-vue/es/locale/lang/en-us';
-import zh from '@arco-design/web-vue/es/locale/lang/zh-CN';
 export const useStore = defineStore('main', {
   state: () => ({
     count: 0,
     theme: false,//主题切换  true ：亮色， false : 暗色
     locales: 'zh',//语言切换 true ：'zh'， false : 'en'
-    language:zh,
+    // language:zh,
   }),
   actions: {
     increment() {
@@ -17,11 +15,11 @@ export const useStore = defineStore('main', {
     },
     localesChange(value: string) {
       this.locales = value
-      if (value=='zh') {
-        this.language = zh
-      } else {
-        this.language = en
-      }
+      // if (value=='zh') {
+      //   this.language = zh
+      // } else {
+      //   this.language = en
+      // }
     },
   },
 })
