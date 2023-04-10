76c41a0c2c6945a4c46a53f23c89d165573f990f
xiaokun-akun <3208377314@qq.com>
2023-04-10 22:03:10 +0800

    88

--- src/views/online.vue
+++ src/views/online.vue
@@ -23,35 +23,35 @@ const key = "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";
 const model = "text-davinci-003"; // ChatGPT 模型名称
 const sendMessage = () => {
   // 向ChatGPT API发送请求并处理响应
-  axios
-    .post(
-      "https://api.openai.com/v1/chat/completions",
-      // "https://api.openai.com/v1/completions",
-      // "https://service-0wiwmlkg-1315488797.usw.apigw.tencentcs.com/v1/chat/completions"
-      {
-        messages: [
-          { role: "system", content: key },
-          { role: "user", content: inputMessage.value },
-          { role: "assistant", content: model } // 添加一个包含 model 参数的对象
-        ],
-      },
-      {
-        headers: {
-          "Authorization": `Bearer ${key}`, // 替换为您的ChatGPT API密钥
-          "Content-Type": "application/json",
-          "model":model // 替换为您要使用的 ChatGPT 模型名称
-        },
-      }
-    )
-    .then((response) => {
-      // 提取并处理ChatGPT API的响应
-      const reply = response.data.choices[0].text;
-      messages.value.push({ sender: "ChatGPT", content: reply });
-      inputMessage.value = "";
-    })
-    .catch((error) => {
-      console.error(error);
-    });
+  // axios
+  //   .post(
+  //     "https://api.openai.com/v1/chat/completions",
+  //     // "https://api.openai.com/v1/completions",
+  //     // "https://service-0wiwmlkg-1315488797.usw.apigw.tencentcs.com/v1/chat/completions"
+  //     {
+  //       messages: [
+  //         { role: "system", content: key },
+  //         { role: "user", content: inputMessage.value },
+  //         { role: "assistant", content: model } // 添加一个包含 model 参数的对象
+  //       ],
+  //     },
+  //     {
+  //       headers: {
+  //         "Authorization": `Bearer ${key}`, // 替换为您的ChatGPT API密钥
+  //         "Content-Type": "application/json",
+  //         "model":model // 替换为您要使用的 ChatGPT 模型名称
+  //       },
+  //     }
+  //   )
+  //   .then((response) => {
+  //     // 提取并处理ChatGPT API的响应
+  //     const reply = response.data.choices[0].text;
+  //     messages.value.push({ sender: "ChatGPT", content: reply });
+  //     inputMessage.value = "";
+  //   })
+  //   .catch((error) => {
+  //     console.error(error);
+  //   });
 };
 </script>
 
